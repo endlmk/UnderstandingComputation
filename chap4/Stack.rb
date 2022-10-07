fbbc96232c75213b2f4e387cbab0ad64c3edb5ed
endlmk <1269467+endlmk@users.noreply.github.com>
2022-10-07 21:45:44 +0900

    Impl stack.

--- chap4/Stack.rb
+++ chap4/Stack.rb
@@ -2,4 +2,12 @@ class Stack < Struct.new(:contents)
   def top
     contents.first
   end
+
+  def pop
+    Stack.new(contents.drop(1))
+  end
+
+  def push(elem)
+    Stack.new(contents.unshift(elem))
+  end
 end
--- chap4/test_stack.rb
+++ chap4/test_stack.rb
@@ -2,8 +2,19 @@ require('minitest/autorun')
 require_relative('Stack')
 
 class StackTest < MiniTest::Test
+  def setup
+    @stack = Stack.new(['a', 'b', 'c', 'd', 'e'])
+  end
   def test_topで最上位を取得できる
-    stack = Stack.new(['a', 'b', 'c', 'd', 'e'])
-    assert_equal('a', stack.top)
+    assert_equal('a', @stack.top)
+  end
+
+  def test_popで要素を取り出せる
+    assert_equal('c', @stack.pop.pop.top)
+  end
+
+  def test_pushで要素をスタックに積める
+    assert_equal('y', @stack.push('x').push('y').top)
+    assert_equal('x', @stack.push('x').push('y').pop.top)
   end
 end
