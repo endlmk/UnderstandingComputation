bb7adf8e7d653a5e5151f7d07cfda66cde432a24
endlmk <1269467+endlmk@users.noreply.github.com>
2022-10-17 21:08:45 +0900

    Refactor.

--- chap4/DPDARulebook.rb
+++ chap4/DPDARulebook.rb
@@ -1,5 +1,9 @@
 class DPDARulebook < Struct.new(:rules)
   def next_configuration(configuration, charactor)
-    rules.detect { |rule| rule.applies_to?(configuration, charactor) }&.follow(configuration)
+    rules_for(configuration, charactor)&.follow(configuration)
+  end
+
+  def rules_for(configuration, charactor)
+    rules.detect { |rule| rule.applies_to?(configuration, charactor) }
   end
 end
