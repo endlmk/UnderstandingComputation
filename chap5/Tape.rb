6bfe7b43f1ec1078729d095baa03996e17b90a7c
endlmk <1269467+endlmk@users.noreply.github.com>
2023-01-16 23:39:01 +0900

    Implement Tape.

--- chap5/Tape.rb
+++ chap5/Tape.rb
@@ -1,2 +1,17 @@
 class Tape < Struct.new(:left, :middle, :right, :blank)
+  def inspect
+    "#<Tape #{left.join}(#{middle})#{right.join}>"
+  end
+
+  def write(character)
+    Tape.new(left, character, right, blank)
+  end
+
+  def move_head_left
+    Tape.new(left[0..-2], left.last || blank, [middle] + right, blank)
+  end
+
+  def move_head_right
+    Tape.new(left + [middle], right.first || blank, right[0..-2], blank)
+  end
 end
--- chap5/test_Tape.rb
+++ chap5/test_Tape.rb
@@ -4,6 +4,30 @@ require_relative('Tape')
 class TapeTest < MiniTest::Test
   def test_テープヘッドの下を読める
     tape = Tape.new(['1', '0', '1'], '1', [], '_')
-    assert_equal('1' ,tape.middle)
+    assert_equal('1', tape.middle)
+  end
+
+  def test_ヘッドを動かせる
+    tape = Tape.new(['1', '0', '1'], '1', [], '_')
+
+    assert_equal(['1', '0'], tape.move_head_left.left)
+    assert_equal('1', tape.move_head_left.middle)
+    assert_equal(['1'], tape.move_head_left.right)
+
+    assert_equal(['1', '0', '1', '1'], tape.move_head_right.left)
+    assert_equal('_', tape.move_head_right.middle)
+    assert_equal([], tape.move_head_right.right)
+  end
+
+  def test_ヘッドの下に書き込める
+    tape = Tape.new(['1', '0', '1'], '1', [], '_')
+
+    assert_equal(['1', '0', '1'], tape.write('0').left)
+    assert_equal('0', tape.write('0').middle)
+    assert_equal([], tape.write('0').right)
+
+    assert_equal(['1', '0', '1', '1'], tape.move_head_right.write('0').left)
+    assert_equal('0', tape.move_head_right.write('0').middle)
+    assert_equal([], tape.move_head_right.write('0').right)
   end
 end
