4c923bb4e9abf002e5a19709c60620efed4c496f
endlmk <1269467+endlmk@users.noreply.github.com>
2023-02-16 23:05:14 +0900

    Impl big step expression. Move small steps.

---
+++ chap2/BigStep/test_Expression.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require_relative 'Number'
+require_relative 'Variable'
+require_relative 'LessThan'
+require_relative 'Add'
+
+class ExpressionTest < MiniTest::Test
+  def test_式を評価できる
+    assert_equal('<<23>>', Number.new(23).evaluate({}).inspect)
+
+    assert_equal('<<23>>', Variable.new(:x).evaluate({ x: Number.new(23) }).inspect)
+
+    assert_equal('<<true>>', LessThan.new(
+      Add.new(Variable.new(:x), Number.new(2)),
+      Variable.new(:y)
+    ).evaluate({ x: Number.new(2), y: Number.new(5) }).inspect)
+  end
+end
